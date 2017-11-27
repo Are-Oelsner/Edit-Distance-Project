6c25f0be736869602dbc159ce71c72680937ce2e
areo279 <are.oelsner@richmond.edu>
2017-11-27 14:00:19 -0500

    Are -removed java files, worked on section

--- Main.java
+++
@@ -1,9 +0,0 @@
-class Main{
-
-    public static void main(String[] args){
-        AutoCorrect autoCorrect = new AutoCorrect("ShorterLexicon.txt");
-        string input = arg[1];
-        for(int i = 0; i < input.length){
-        }
-    }
-}
---
+++ Section.h
@@ -0,0 +1,10 @@
+#ifndef __SECTION_H__
+#define __SECTION_H__
+
+class Section {
+  private:
+    bool goUp;
+    bool goUp;
+}
+
+#endif
